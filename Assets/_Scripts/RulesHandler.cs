731a89d0c6c69312eadf7e83e186eb081ccda5c5
Grace <grko@ucsc.edu>
2019-03-12 20:58:53 -0700

    add rulebook panel

---
+++ Assets/_Scripts/RulesHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RulesHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject rulesPanel;
+    [SerializeField] private Button closeButton;
+    [SerializeField] private Button rightButton;
+    [SerializeField] private Button leftButton;
+    [SerializeField] private Text rulesText;
+
+    private bool panelOpen;
+    private int pageCounter;
+
+    private void Start()
+    {
+        closeButton.onClick.AddListener(() => OpenClosePanel());
+    }
+
+    public void OpenClosePanel()
+    {
+        if (!panelOpen)
+        {
+            // bring panel to top layer
+            rulesPanel.transform.SetAsLastSibling();
+
+            rulesPanel.SetActive(true);
+            panelOpen = true;
+        }
+        else
+        {
+            rulesPanel.SetActive(false);
+            panelOpen = false;
+        }
+    }
+}
